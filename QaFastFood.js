a3ac4ff97846de077ec2f6c9eb2530c9c9de00fa
Nexssus <102626988+Nexssus@users.noreply.github.com>
2022-08-22 23:29:35 +0200

    Add files via upload

---
+++ QaFastFood.js
@@ -0,0 +1,34 @@
+const webdriver = require('selenium-webdriver');
+const { By, Key, until } = require('selenium-webdriver')
+
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
+
+describe('fastFoodQa', function() {
+    let driver;
+
+    before(function() {
+        driver = new webdriver.Builder().forBrowser('chrome').build();
+    });
+
+    after(async function() {
+        await driver.quit();
+    });
+
+    beforeEach(async function() {
+        // Pokrece se pre svakog testa
+    });
+
+    afterEach(async function() {
+        // Pokrece se nakon svakog testa
+    });
+
+    it("Verify homepage is open", async function(){
+        await driver.get("http://test.qa.rs/");
+        expect(await driver.getCurrentUrl()).to.eq("http://test.qa.rs/");
+    })
+    
+    
+
+});
